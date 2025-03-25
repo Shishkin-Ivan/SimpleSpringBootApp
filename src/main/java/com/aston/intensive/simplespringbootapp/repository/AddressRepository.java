2f401ab8bf4e1145c35bde5ae6fa76d5fab2fac3
ROMUL-SHOW <Craz9.Bunn9@yandex.ru>
2025-03-26 00:22:15 +0300

    feat(repository): extend TicketInfoRepository, ServiceRepository, AttractionRepository, AddressRepository filtering query support

--- src/main/java/com/aston/intensive/simplespringbootapp/repository/AddressRepository.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/repository/AddressRepository.java
@@ -2,13 +2,34 @@ package com.aston.intensive.simplespringbootapp.repository;
 
 import com.aston.intensive.simplespringbootapp.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
-
     Optional<Address> findById(UUID id);
+
+    @Query("SELECT a FROM Address a " +
+            "WHERE (:id IS NULL OR a.id = :id) " +
+            "AND (:city IS NULL OR a.city = :city) " +
+            "AND (:street IS NULL OR a.street = :street) " +
+            "AND (:building IS NULL OR a.building = :building) " +
+            "AND (:region IS NULL OR a.region = :region) " +
+            "AND (:latitude IS NULL OR a.latitude = :latitude) " +
+            "AND (:longitude IS NULL OR a.longitude = :longitude)")
+    List<Address> findByFilters(
+            @Param("id") UUID id,
+            @Param("city") String city,
+            @Param("street") String street,
+            @Param("building") Integer building,
+            @Param("region") String region,
+            @Param("latitude") Double latitude,
+            @Param("longitude") Double longitude
+    );
+
 }
--- src/main/java/com/aston/intensive/simplespringbootapp/repository/TicketInfoRepository.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/repository/TicketInfoRepository.java
@@ -2,10 +2,26 @@ package com.aston.intensive.simplespringbootapp.repository;
 
 import com.aston.intensive.simplespringbootapp.model.TicketInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TicketInfoRepository extends JpaRepository<TicketInfo, UUID> {
+
     Optional<TicketInfo> findById(UUID id);
+
+    @Query("SELECT a FROM TicketInfo a " +
+            "WHERE (:id IS NULL OR a.id = :id) " +
+            "AND (:price IS NULL OR a.price = :price) " +
+            "AND (:currency IS NULL OR a.currency = :currency) " +
+            "AND (:availability IS NULL OR a.availability = :availability)")
+    List<TicketInfo> findByFilters(
+            @Param("id") UUID id,
+            @Param("price") Double price,
+            @Param("currency") String currency,
+            @Param("availability") Boolean availability
+    );
 }
