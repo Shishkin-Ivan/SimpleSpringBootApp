f55265d397d748c1bbf7fcaa6f38395b7d7e3da3
ROMUL-SHOW <Craz9.Bunn9@yandex.ru>
2025-03-26 00:17:05 +0300

    feat(service): extend TicketInfoService, ServiceService, AttractionService, AddressService with CRUD, filtering, and response DTO update

--- src/main/java/com/aston/intensive/simplespringbootapp/service/ServiceService.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/service/ServiceService.java
@@ -1,9 +1,23 @@
 package com.aston.intensive.simplespringbootapp.service;
 
 import com.aston.intensive.simplespringbootapp.dto.ServiceRequestDTO;
+import com.aston.intensive.simplespringbootapp.dto.ServiceResponseDTO;
+import com.aston.intensive.simplespringbootapp.model.ServiceType;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ServiceService {
-    ServiceRequestDTO findById(UUID id);
+
+    ServiceResponseDTO getServiceById(UUID id);
+
+    List<ServiceResponseDTO> getAllServices(int pageNumber, int pageSize, UUID id, String name, String description, ServiceType type);
+
+    long getServiceCount();
+
+    ServiceResponseDTO createService(ServiceRequestDTO serviceRequestDTO);
+
+    ServiceResponseDTO updateService(UUID id, ServiceRequestDTO serviceRequestDTO);
+
+    void deleteService(UUID id);
 }
