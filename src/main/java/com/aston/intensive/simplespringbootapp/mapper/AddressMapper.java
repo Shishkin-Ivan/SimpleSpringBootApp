842f61614fa19e02dc462a8290913479d1933da8
ROMUL-SHOW <Craz9.Bunn9@yandex.ru>
2025-03-26 00:27:38 +0300

    feat(mapper): extend AddressMapper, AttractionMapper, ServiceMapper, TicketInfoMapper with response DTO mapping and additional fields

--- src/main/java/com/aston/intensive/simplespringbootapp/mapper/AddressMapper.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/mapper/AddressMapper.java
@@ -1,18 +1,33 @@
 package com.aston.intensive.simplespringbootapp.mapper;
 
 import com.aston.intensive.simplespringbootapp.dto.AddressRequestDTO;
+import com.aston.intensive.simplespringbootapp.dto.AddressResponseDTO;
 import com.aston.intensive.simplespringbootapp.model.Address;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AddressMapper {
 
-    public AddressRequestDTO mapToAddressRequestDTO(Address address) {
-        return AddressRequestDTO.builder()
+    public Address mapToAddress(AddressRequestDTO dto) {
+        Address address = new Address();
+        address.setBuilding(dto.building());
+        address.setCity(dto.city());
+        address.setRegion(dto.region());
+        address.setStreet(dto.street());
+        address.setLatitude(dto.latitude());
+        address.setLongitude(dto.longitude());
+        return address;
+    }
+
+    public AddressResponseDTO mapToAddressResponseDTO(Address address) {
+        return AddressResponseDTO.builder()
+                .id(address.getId())
                 .building(address.getBuilding())
                 .city(address.getCity())
                 .region(address.getRegion())
                 .street(address.getStreet())
+                .latitude(address.getLatitude())
+                .longitude(address.getLongitude())
                 .build();
     }
 }
