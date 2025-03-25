9dcdf15b56a00725b5c993c996edcc58c5e85cf9
ROMUL-SHOW <Craz9.Bunn9@yandex.ru>
2025-03-26 00:23:00 +0300

    feat(controller): add CRUD and filtering to multiple controllers, switch to ResponseDTO

--- src/main/java/com/aston/intensive/simplespringbootapp/controller/ServiceController.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/controller/ServiceController.java
@@ -1,14 +1,37 @@
 package com.aston.intensive.simplespringbootapp.controller;
 
 import com.aston.intensive.simplespringbootapp.dto.ServiceRequestDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.aston.intensive.simplespringbootapp.dto.ServiceResponseDTO;
+import com.aston.intensive.simplespringbootapp.model.ServiceType;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/api/service")
 public interface ServiceController {
+
     @GetMapping("/{id}")
-    ServiceRequestDTO getServiceById(@PathVariable UUID id);
+    ServiceResponseDTO getServiceById(@PathVariable UUID id);
+
+    @GetMapping
+    List<ServiceResponseDTO> getAllServices(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "3") int pageSize,
+            @RequestParam(required = false) UUID id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) ServiceType type
+    );
+
+    @GetMapping("/count")
+    long getServiceCount();
+
+    @PostMapping
+    ServiceResponseDTO createService(@RequestBody ServiceRequestDTO serviceRequestDTO);
+
+    @PutMapping("/{id}")
+    ServiceResponseDTO updateService(@PathVariable UUID id, @RequestBody ServiceRequestDTO serviceRequestDTO);
+
+    @DeleteMapping("/{id}")
+    void deleteService(@PathVariable UUID id);
 }
--- src/main/java/com/aston/intensive/simplespringbootapp/controller/ServiceControllerImpl.java
+++ src/main/java/com/aston/intensive/simplespringbootapp/controller/ServiceControllerImpl.java
@@ -1,19 +1,55 @@
 package com.aston.intensive.simplespringbootapp.controller;
 
 import com.aston.intensive.simplespringbootapp.dto.ServiceRequestDTO;
+import com.aston.intensive.simplespringbootapp.dto.ServiceResponseDTO;
+import com.aston.intensive.simplespringbootapp.model.ServiceType;
 import com.aston.intensive.simplespringbootapp.service.ServiceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
+@RequestMapping("/api/service")
 @RestController
 @RequiredArgsConstructor
 public class ServiceControllerImpl implements ServiceController {
     private final ServiceService serviceService;
 
     @Override
-    public ServiceRequestDTO getServiceById(UUID id) {
-        return serviceService.findById(id);
+    public ServiceResponseDTO getServiceById(@PathVariable UUID id) {
+        return serviceService.getServiceById(id);
+    }
+
+    @Override
+    public List<ServiceResponseDTO> getAllServices(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "3") int pageSize,
+            @RequestParam(required = false) UUID id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) ServiceType type) {
+
+        return serviceService.getAllServices(pageNumber, pageSize, id, name, description, type);
+    }
+
+    @Override
+    public long getServiceCount() {
+        return serviceService.getServiceCount();
+    }
+
+    @Override
+    public ServiceResponseDTO createService(@RequestBody ServiceRequestDTO serviceRequestDTO) {
+        return serviceService.createService(serviceRequestDTO);
+    }
+
+    @Override
+    public ServiceResponseDTO updateService(@PathVariable UUID id, @RequestBody ServiceRequestDTO serviceRequestDTO) {
+        return serviceService.updateService(id, serviceRequestDTO);
+    }
+
+    @Override
+    public void deleteService(@PathVariable UUID id) {
+        serviceService.deleteService(id);
     }
 }
